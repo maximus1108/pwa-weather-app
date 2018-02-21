9f53a080c8a1be74a17fc000cdadc0d7fe58c8ff
maximus1108 <max.perry1108@yahoo.co.uk>
2018-02-21 14:21:40 +0000

    functionality added to load cached data then update with server response after

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -171,6 +171,25 @@
     var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
         statement;
     // TODO add cache logic here
+    if ('caches' in window) {
+      /*
+       * Check if the service worker has already cached this city's weather
+       * data. If the service worker has the data, then display the cached
+       * data while the app fetches the latest data.
+       */
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function updateFromCache(json) {            
+            var results = json.query.results;
+            console.log('cahceeee',results)
+            results.key = key;
+            results.label = label;
+            results.created = json.query.created;
+            app.updateForecastCard(results);
+          });
+        }
+      });
+    }
 
     // Fetch the latest data.
     var request = new XMLHttpRequest();
@@ -179,6 +198,7 @@
         if (request.status === 200) {
           var response = JSON.parse(request.response);
           var results = response.query.results;
+          console.log('serveeee', results)
           results.key = key;
           results.label = label;
           results.created = response.query.created;
